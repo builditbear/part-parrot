f45eac74cecec5df4e431e88239fa04d0bf15519
builditbear <brandon.na.chavez@gmail.com>
2022-01-19 16:38:49 -0800

    Delete now obsolete testing code from Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,20 +17,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("/views/MainScreen.fxml"));
-//        stage.setTitle("main.Main Screen");
-//        stage.setScene(new Scene(root, 900, 500));
-//        stage.show();
         loadScene(stage, "Main Screen", "MainScreen", "490x940");
-
 
         //Test code for main.DataModel.
         ObservableList<DataModel.Part> parts = DataModel.Inventory.getAllParts();
         DataModel.generateParts(parts, 12);
-//        for(DataModel.Part p : DataModel.Inventory.getAllParts()) {
-//            System.out.println(p.getName());
-//        }
-        System.out.println(DataModel.Inventory.lookupPart(2));
     }
 
     private void loadScene(Stage stage, String title, String view, String resolution) throws IOException {
