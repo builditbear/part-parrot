fe7300ca86b2ad3993aca0df2be2e5848941116b
builditbear <brandon.na.chavez@gmail.com>
2022-01-19 16:37:16 -0800

    Update MainScreen view to be scalable

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,15 +11,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javax.xml.crypto.Data;
+import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/MainScreen.fxml"));
-        stage.setTitle("main.Main Screen");
-        stage.setScene(new Scene(root, 900, 500));
-        stage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("/views/MainScreen.fxml"));
+//        stage.setTitle("main.Main Screen");
+//        stage.setScene(new Scene(root, 900, 500));
+//        stage.show();
+        loadScene(stage, "Main Screen", "MainScreen", "490x940");
+
 
         //Test code for main.DataModel.
         ObservableList<DataModel.Part> parts = DataModel.Inventory.getAllParts();
@@ -29,4 +33,12 @@ public class Main extends Application {
         System.out.println(DataModel.Inventory.lookupPart(2));
     }
 
+    private void loadScene(Stage stage, String title, String view, String resolution) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/" + view + ".fxml"));
+        String[] dimensions = resolution.split("x");
+        Scene scene = new Scene(root, Integer.parseInt(dimensions[1]), Integer.parseInt(dimensions[0]));
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
